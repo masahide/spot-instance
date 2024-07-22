622d330379f65543320dc8c194136d6dff602e4e
YAMASAKI Masahide <masahide.y@gmail.com>
2024-07-22 22:41:22 +0900

    fix

--- lib/spot-instance-stack.ts
+++ lib/spot-instance-stack.ts
@@ -10,6 +10,15 @@ export class SpotInstanceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const RECORD_NAME = "p.suzu.me.uk";
+    const ZONE_ID = "Z08397422LH3IDIZRIAIU";
+    const TTL = "300";
+    const KEYPAIR = "masahide ed25519";
+    const SPOTPRICE = "0.004";
+    const ASG_NAME = "spotInstance01";
+    const INTERVAL = "900";
+    const discordWebhookUrl = process.env.DISCORD_WEBHOOK || "";
+
     const vpc = new ec2.Vpc(this, "VPC", {
       maxAzs: 3,
       natGateways: 0,
@@ -36,13 +45,27 @@ export class SpotInstanceStack extends cdk.Stack {
         iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonRoute53FullAccess"),
       ],
     });
+    role.addToPolicy(
+      new iam.PolicyStatement({
+        actions: ["autoscaling:SetDesiredCapacity"],
+        resources: ["*"],
+      })
+    );
 
     const userDataScript = fs.readFileSync(path.join(__dirname, "../user-data.sh"), "utf8");
+    let updatedUserDataScript = userDataScript
+      .replace("YOUR_DISCORD_WEBHOOK_URL", discordWebhookUrl)
+      .replace("$ASG_NAME", ASG_NAME)
+      .replace("$ZONE_ID", ZONE_ID)
+      .replace("$RECORD_NAME", RECORD_NAME)
+      .replace("$INTERVAL", INTERVAL)
+      .replace("$TTL", TTL);
     const userData = ec2.UserData.forLinux();
-    userData.addCommands(userDataScript);
+    userData.addCommands(updatedUserDataScript);
 
     const spotFleet = new autoscaling.AutoScalingGroup(this, "SpotFleet", {
       vpc,
+      autoScalingGroupName: ASG_NAME,
       maxCapacity: 1,
       minCapacity: 0,
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T4G, ec2.InstanceSize.NANO),
@@ -53,9 +76,9 @@ export class SpotInstanceStack extends cdk.Stack {
       securityGroup,
       role,
       // desiredCapacity: 0,
-      spotPrice: "0.004",
+      spotPrice: SPOTPRICE,
       userData,
-      keyPair: ec2.KeyPair.fromKeyPairName(this, "KeyPair", "masahide ed25519"),
+      keyPair: ec2.KeyPair.fromKeyPairName(this, "KeyPair", KEYPAIR),
     });
 
     new cdk.CfnOutput(this, "AutoScalingGroupName", {
--- user-data.sh
+++ user-data.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-set -e
+set -xe
 
-dnf update -y
-dnf install -y aws-cli
+#dnf update -y
+#dnf install -y aws-cli
 
-INTERVAL=600
-IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
-ZONE_ID="Z08397422LH3IDIZRIAIU"
-RECORD_NAME="p.suzu.me.uk"
-TTL=300
+TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+IP=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/public-ipv4)
+DISCORD_WEBHOOK_URL="YOUR_DISCORD_WEBHOOK_URL"
+RECORD="$RECORD_NAME"
 
 cat <<EOF >change-batch.json
 {
@@ -16,7 +15,7 @@ cat <<EOF >change-batch.json
     {
       "Action": "UPSERT",
       "ResourceRecordSet": {
-        "Name": "$RECORD_NAME",
+        "Name": "$RECORD",
         "Type": "A",
         "TTL": $TTL,
         "ResourceRecords": [
@@ -30,27 +29,40 @@ cat <<EOF >change-batch.json
 }
 EOF
 
-aws route53 change-resource-record-sets --hosted-zone-id $ZONE_ID --change-batch file://change-batch.json
-
-get_rx_bytes() {
-    total_rx=0
-    for pid in $(pidof sshd); do
-        file=/proc/${pid}/net/dev
-        if [[ -f $file ]]; then
-            rx=$(awk '{if (NR>2) sum+=$2} END {print sum}' "${file}")
-            total_rx=$((total_rx + rx))
-        fi
-    done
-    echo $total_rx
+send_discord_notification() {
+  MESSAGE=$1
+  PAYLOAD=$(jq -n --arg content "$MESSAGE" '{content: $content}')
+  curl -H "Content-Type: application/json" -X POST -d "$PAYLOAD" $DISCORD_WEBHOOK_URL
 }
 
-initial_rx=$(get_rx_bytes)
-while true; do
+aws route53 change-resource-record-sets --hosted-zone-id $ZONE_ID --change-batch file://change-batch.json
+send_discord_notification "$RECORD IP: $IP が起動しました"
+
+get_rx_bytes() {
+  total_rx=0
+  for pid in $(pidof sshd); do
+    file=/proc/${pid}/net/dev
+    if [[ -f $file ]]; then
+      rx=$(awk '{if (NR>2) sum+=$2} END {print sum}' "${file}")
+      total_rx=$((total_rx + rx))
+    fi
+  done
+  echo $total_rx
+}
+
+monitor_ssh_connections() {
+  initial_rx=$(get_rx_bytes)
+  while true; do
     sleep $INTERVAL
     current_rx=$(get_rx_bytes)
     received=$((current_rx - initial_rx))
     if [[ $received -eq 0 ]]; then
-        aws autoscaling set-desired-capacity --auto-scaling-group-name ASG_NAME --desired-capacity 0
+      send_discord_notification "$RECORD IP: $IP をストップしました"
+      aws autoscaling set-desired-capacity --auto-scaling-group-name $ASG_NAME --desired-capacity 0
     fi
     initial_rx=$current_rx
-done
+  done
+}
+
+set +xe
+monitor_ssh_connections &
